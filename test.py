268fda2314e61fc5626f326cfca96b7393101bd0
venividivici2003 <3vdataconsult@gmail.comm>
2020-07-14 09:49:23 -0700

    introduction to python for data science

---
+++ test.py
@@ -0,0 +1,11 @@
+import numpy as np
+import seaborn; seaborn.set()
+import matplotlib.pyplot as plt
+from scipy import special
+from scipy import stats
+import pandas as pd
+
+b = np.arange(3)
+print(b)
+c = np.arange(3)[np.newaxis, :]
+print(c)
